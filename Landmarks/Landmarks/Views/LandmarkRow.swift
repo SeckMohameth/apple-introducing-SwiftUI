84fffa572e356ed9a587a635c7c59386244a1819
SeckMohameth <mohamethseck34@gmail.com>
2024-01-16 22:20:54 -0500

    workign with previews

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Mohameth Seck on 1/16/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
