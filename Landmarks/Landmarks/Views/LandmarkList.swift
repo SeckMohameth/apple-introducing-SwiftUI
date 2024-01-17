15b110c2c0511ff0eee8fa92141cf033d766d726
SeckMohameth <mohamethseck34@gmail.com>
2024-01-16 22:26:20 -0500

    workng with lists

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Mohameth Seck on 1/16/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
