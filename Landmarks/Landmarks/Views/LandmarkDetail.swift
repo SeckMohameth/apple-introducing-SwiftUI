fff6ef0aff671b8f8fb20a7fa3bf714ef507bdb5
SeckMohameth <mohamethseck34@gmail.com>
2024-01-16 23:54:49 -0500

    completed list and navigation

---
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Mohameth Seck on 1/16/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
