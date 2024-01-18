97ff98705cfa26467bf0babf40bff8a0bad2f884
SeckMohameth <mohamethseck34@gmail.com>
2024-01-17 22:47:08 -0500

    using state to control which places are favorited

--- HandlingUserInput/StartingPoint/Landmarks/Views/LandmarkList.swift
+++ HandlingUserInput/StartingPoint/Landmarks/Views/LandmarkList.swift
@@ -8,15 +8,30 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
+    
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationSplitView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach (filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
         } detail: {
             Text("Select a Landmark")
