69d1189629b2df4268a80012e40fed6dd00a6247
SeckMohameth <mohamethseck34@gmail.com>
2024-01-18 21:52:17 -0500

    failed to implement [Tutorial Outdated?]

---
+++ ComposingComplexInterfaces/StartingPoint/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Mohameth Seck on 1/18/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
---
+++ ComposingComplexInterfaces/StartingPoint/Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Mohameth Seck on 1/18/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
